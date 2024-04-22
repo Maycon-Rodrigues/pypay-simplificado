ff4492431f8800175e21c525c278a078cb3da984
Maycon-Rodrigues <maycon.zng@gmail.com>
2024-04-22 13:47:53 -0300

    add class based view

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from . import views
+from .views import PersonListAndCreate, PersonDetailAndUpdatePartial, TransactionListAndCreate
 
 urlpatterns = [
     #person_route
-    path("v1/persons", views.person, name="person_get_or_post"),
-    path("v1/person/<int:pk>", views.person_detail, name="person_detail"),
-    path("v1/person/<int:pk>/deposit", views.deposit, name="deposit"),
+    path("v1/persons", PersonListAndCreate.as_view(), name="person_list_and_post"),
+    path("v1/person/<int:pk>", PersonDetailAndUpdatePartial.as_view(), name="person_detail"),
+    path("v1/person/<int:pk>/deposit", PersonDetailAndUpdatePartial.as_view(), name="deposit"),
 
     #transaction_route
-    path("v1/transactions", views.transaction, name="transaction_get_or_post"),
+    path("v1/transactions", TransactionListAndCreate.as_view(), name="transaction_get_or_post"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,58 +1,60 @@
+import requests
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import requests
+from rest_framework.views import APIView
 
 from .models import Person, Transaction
 from .serializers import PersonSerializer, TransactionSerializer
 
 
-@api_view(["GET", "POST"])
-def person(request):
-    if request.method == "GET":
-        serializer = PersonSerializer(Person.objects.all(), many=True)
+class PersonListAndCreate(APIView):
+    def get(self, request):
+        people = Person.objects.all()
+        serializer = PersonSerializer(people, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+    
+    def post(self, request):
         serializer = PersonSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PersonDetailAndUpdatePartial(APIView):
+    def get(self, request, pk):
+        try:
+            person = Person.objects.get(pk=pk)
+        except Person.DoesNotExist:
+            return Response({"message": "Usuário não encontrado!"}, status=status.HTTP_404_NOT_FOUND)
+    
+        serializer = PersonSerializer(person)
+
+        return Response(serializer.data)
+    
+    # Deposit
+    def patch(self, request, pk):
+        person = Person.objects.get(pk=pk)
+        person.balance += request.data.get("balance")
+        serializer = PersonSerializer(person, data=person.__dict__, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET"])
-def person_detail(request, pk):
-    try:
-        person = Person.objects.get(pk=pk)
-    except Person.DoesNotExist:
-        return Response({"message": "Usuário não encontrado!"}, status=status.HTTP_404_NOT_FOUND)
-    
-    serializer = PersonSerializer(person)
-
-    return Response(serializer.data)
-
-
-@api_view(["PATCH"])
-def deposit(request, pk):
-    person = Person.objects.get(pk=pk)
-    person.balance += request.data.get("balance")
-    serializer = PersonSerializer(person, data=person.__dict__, partial=True)
-
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["GET", "POST"])
-def transaction(request):
-    transaction = Transaction.objects.all()
-    if request.method == "GET":
+class TransactionListAndCreate(APIView):
+    def get(self, request):
+        transaction = Transaction.objects.all()
         serializer = TransactionSerializer(transaction, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+    
+    
+    def post(self, request):
         serializer = TransactionSerializer(data=request.data)
         if serializer.is_valid():
             req = requests.get('https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc').json()
@@ -63,3 +65,4 @@ def transaction(request):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
